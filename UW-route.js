2fe99a5ab5533f606375330ad12a6a33cb43720a
Daniel Wang <s92025592025@gmail.com>
2017-02-07 13:20:34 -0800

    setup instruction for getAbsoluteLocation

--- UW-route.js
+++ UW-route.js
@@ -2,20 +2,18 @@
 // @Description: This module is mainly used to analysis the location relationship on campus
 // 2017/01/31
 
-/* For testing and reference purpose
+/* For testing and reference purpose */
 
-// constructor
-var UWRoute = function (name){
-	this.name = name;
+
+var UWRoute = function (apiKey){
+	this._apiKey = apiKey;
+	this.BASE = 'https://maps.googleapis.com/maps/api/';
 };
 
 
-// instance function
-UWRoute.prototype.testing1 = function (age){
-	console.log('Hi!, my name is ' + this.name + 'and I\'m ' +
-				age + ' years old');
-}
+// pre: should give abbr the abbreviation of the building(without classroom num)
+// post: will return a set of absolution in json format {lat, lng}
+UWRoute.prototype.getAbsoluteLocation = function (abbr){}
 
 // export
 module.exports = UWRoute;
-*/
