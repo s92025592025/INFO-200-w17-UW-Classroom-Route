09648c6d8d25b054b82b313e9dc052b5b1a44eac
Daniel Wang <s92025592025@gmail.com>
2017-02-07 13:54:16 -0800

    Fix bugs in getAbsoluteLocation()

--- UW-route.js
+++ UW-route.js
@@ -8,19 +8,21 @@ const fs = require('file-system');
 var UWRoute = function (apiKey){
 	this._apiKey = apiKey;
 	this.BASE = 'https://maps.googleapis.com/maps/api/';
-	this.buildingList = JSON.parse(__dirname + '/data/testing')
+	this.buildingList = JSON.parse(fs.readFileSync(__dirname + '/data/testing_building_list.json'));
 };
 
 
 // pre: should give abbr the abbreviation of the building(without classroom num)
 // post: will return a set of absolution in json format {lat, lng}
 UWRoute.prototype.getAbsoluteLocation = function (abbr){
-	for(var i = 0; i < buildingList.features.length; i++){
-		if(buildingList.features[i].attributes.faccode == abbr){
-			return buildingList.features[i].geometry.coordinates;
+	for(var i = 0; i < this.buildingList.features.length; i++){
+		if(this.buildingList.features[i].attributes.faccode == abbr){
+			return this.buildingList.features[i].geometry.coordinates;
 		}
 	}
-}
+
+	return "Not yet updated";
+};
 
 // export
 module.exports = UWRoute;
