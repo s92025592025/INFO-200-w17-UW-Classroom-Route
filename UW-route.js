71cb274f88653aef0078aba093430bb233591776
Daniel Wang <s92025592025@gmail.com>
2017-02-07 17:39:31 -0800

    Add getAddress

--- UW-route.js
+++ UW-route.js
@@ -4,6 +4,7 @@
 'use strict';
 /* For testing and reference purpose */
 const fs = require('file-system');
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 var UWRoute = function (apiKey){
 	this._apiKey = apiKey;
@@ -16,7 +17,7 @@ var UWRoute = function (apiKey){
 // post: will return a set of absolution in json format {lat, lng}
 UWRoute.prototype.getAbsoluteLocation = function (abbr){
 	for(var i = 0; i < this.buildingList.features.length; i++){
-		if(this.buildingList.features[i].attributes.faccode == abbr){
+		if(this.buildingList.features[i].attributes.faccode == abbr.toUpperCase()){
 			if(this.buildingList.features[i].geometry.hasOwnProperty('coordinates')){
 				return this.buildingList.features[i].geometry.coordinates;
 			}
@@ -25,6 +26,31 @@ UWRoute.prototype.getAbsoluteLocation = function (abbr){
 
 	return "Not yet updated";
 };
+
+// pre: should give abbr the abbreviation of the building(without classroom num)
+// post: will return a address of the building
+UWRoute.prototype.getAddress = function (abbr){
+	var absolute = this.getAbsoluteLocation(abbr);
+	var address = "No Result";
+	if(absolute == "Not yet updated"){
+		return absolute;
+	}
+
+	var response = new XMLHttpRequest();
+	response.open('GET', this.BASE + 'geocode/json' + 
+					'?key=' + this._apiKey + 
+					'&latlng=' + absolute.lat + ',' + absolute.lng, false);
+	response.onload = function(){
+		var response = JSON.parse(this.responseText);
+		if(response.results.length > 0){
+			address = response.results[0].formatted_address;
+		}
+	};
+
+	response.send();
+
+	return address;
+}
 
 // export
 module.exports = UWRoute;
